c0108dc0830600de35a9eca83ad60edf2f549e4d
Gregory Bai <gregorybai@outlook.com>
2021-02-11 00:36:55 +0300

    init project

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>hi</h2>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
