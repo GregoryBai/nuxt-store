71cebfc4d239e295b5c393278f18ac8b37b6baae
Gregory Bai <gregorybai@outlook.com>
2021-02-17 13:16:48 +0300

    prepare for a delpoy

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,9 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
+  target: 'static',
+  router: {
+    base: '/nuxt-store/',
+  },
   head: {
     title: 'nuxt',
     htmlAttrs: {
